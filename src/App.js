c9b055d8497368a5b4f883d9de952e8f41db3fc4
sharadbhat-98 <sharathbhat563@gmail.com>
2020-12-12 13:39:25 +0530

    siginup page

--- src/App.js
+++ src/App.js
@@ -54,7 +54,7 @@ const App = () => {
 
     //validation of fields using JOI
     const schema = {
-        name: Joi.string().required().min(6),
+        name: Joi.string().required(),
         password: Joi.string().required(),
         email: Joi.string().required()
             .email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } }),
